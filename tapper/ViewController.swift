ef8c4d2c41ce793188677036fa43874291d12cee
ryanbbernstein <ryanbbernstein@gmail.com>
2016-02-03 12:04:07 -0800

    Working version

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numTaps = 0
+    var currentTaps = 0
+    
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var tapBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func playBtnPressed(sender: UIButton!) {
+        numTaps = Int(arc4random_uniform(50))
+        //print(numTaps)
+        playBtn.hidden = true
+        tapBtn.hidden = false
+        currentTaps = 0
+    }
+    @IBAction func coinTapped(sender: AnyObject) {
+        currentTaps++
+        if currentTaps >= numTaps {
+            numTaps = 0
+            tapBtn.hidden = true
+            playBtn.hidden = false
+        }
+    }
 }
-
